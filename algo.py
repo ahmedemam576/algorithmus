fabf907ef44735e4c63921c1ba923b343aceb548
Ahmed Emam <58878570+ahmedemam576@users.noreply.github.com>
2023-11-24 18:31:50 +0100

    Update algo.py
    construct a BST from preorder and inorder

--- algo.py
+++ algo.py
@@ -105,4 +105,27 @@ class Codec:
             root.right =dfs()
             return root
         return dfs()
+	    ### construct BST from preorder and inorder
+class Solution:
+   def buildTree(self, preorder, inorder):
+        """
+        :type preorder: List[int]
+        :type inorder: List[int]
+        :rtype: TreeNode
+        """
+        
+        # Recursive solution
+        if inorder:   
+            # Find index of root node within in-order traversal
+            index = inorder.index(preorder.pop(0))
+            root = TreeNode(inorder[index])
+            
+            # Recursively generate left subtree starting from 
+            # 0th index to root index within in-order traversal
+            root.left = self.buildTree(preorder, inorder[:index])
+            
+            # Recursively generate right subtree starting from 
+            # next of root index till last index
+            root.right = self.buildTree(preorder, inorder[index+1:])
+            return root
 			
